6dfe22db37ffd815b48d8f59ac4c5b2dd941347b
saidolim10 <git001hub@gmail.com>
2024-11-24 15:17:26 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let obj = {
+
+}
+
+obj.name = prompt('ismingizni yozing')
+obj.surname = prompt('familiangizni yozing')
+obj.spaceId = prompt('spacedagi idingizni yozing')
+obj.spaceParol = prompt('spacedagi parolingizni yozing')
+obj.tel = prompt('telefon raqamingizni yozing')
+obj.coin = prompt('space dagi coiningizni yozing')
+
+
+
+
+console.log(obj);
